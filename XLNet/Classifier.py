cf7776e7075b36c0b580e9b525645c5404f7f7ab
Chen Wang <blcuwc@gmail.com>
2019-11-12 00:56:39 +0100

    add classifier.py

--- XLNet/Classifier.py
+++ XLNet/Classifier.py
@@ -16,6 +16,16 @@ import torch.nn.functional as F
 
 import sys
 
+disease_ids = ['DIS00', 'DIS01', 'DIS02']
+category = ['factuality', 'polarity']
+
+
+def Set_input_embedding(base_model):
+    # Len of the sentence must be the same as the training model
+    # See model's 'max_position_embeddings' = 512
+    max_len  = 64
+    # With cased model, set do_lower_case = False
+    tokenizer = XLNetTokenizer(vocab_file = base_model, do_lower_case = False)
 
 def _3_fold_cv():
     return train_set, test_set
@@ -23,11 +33,24 @@ def _3_fold_cv():
 def _5_fold_cv():
     return train_set, test_set
 
-def Load_data(dataset):
-    return 0
+def Load_data(dataset_dir, base_model):
+    #read every dataset in dataset_dir into dataframe
+    for dataset_name in os.listdir(dataset_dir):
+        dataset_path = os.path.join(dataset_dir, dataset_name)
+        df_data = pd.read_csv(dataset_path, sep="\t",encoding="utf-8",names=['texts','labels'])
+        print (df_data.columns)
+        df_data = df_data.drop(df_data['labels'] == 'NOT_LABELED')
+        sentences = df_data['texts'].to_list()
+        labels = df_data['labels'].to_list()
+
+
+
+    # set input embedding using base xlnet model
+    
 
 
 if __name__ == '__main__':
-    dataset = sys.argv[1]
-    Load_data(dataset)
+    dataset_dir = sys.argv[1]
+    base_model = sys.argv[2]
+    Load_data(dataset_dir, base_model)
 
--- XLNet/run_classifier.sh
+++ XLNet/run_classifier.sh
@@ -1,1 +1,1 @@
-`CUDA_VISIBLE_DEVICES=1 python Classifier.py processed_dataset/`
+`CUDA_VISIBLE_DEVICES=1 python Classifier.py processed_dataset/ pre_trained_model/xlnet-base-cased-spiece.model`
